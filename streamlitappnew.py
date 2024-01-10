6b5bbd66f3090a36ce4abe989f2b8bd22d49c5c2
haris021 <harismushtaq21@yahoo.com>
2024-01-10 14:27:31 +0500

    Update streamlitappnew.py

--- streamlitappnew.py
+++ streamlitappnew.py
@@ -30,6 +30,8 @@ selection_dict = {  'spei03': 'SPEI_03_month',
                     'spei12': 'SPEI_12_month'  }
 
 
+
+
 st.title("Pakistan Drought Monitor")
 
 divisions_master_df = pd.read_csv("DivisionFileNames.csv")
@@ -38,9 +40,9 @@ division = st.sidebar.selectbox("Select Division", tuple(divisions_master_df["Di
 st.sidebar.divider()
 st.sidebar.subheader("Map Options")
 
-#map_month = st.sidebar.select_slider("Select Month", options= tuple(range(1,13)))
+map_month = st.sidebar.select_slider("Select Month", options= tuple(range(1,13)))
 
-map_month = st.sidebar.selectbox("Select Month", options= tuple(['January','February','March','April','May','June','July','August','September','October','November','December']))
+#map_month = st.sidebar.selectbox("Select Month", options= tuple(['January','February','March','April','May','June','July','August','September','October','November','December']))
 map_year = st.sidebar.select_slider("Select Year", options = tuple(range(1950, 2023)))
 
 
